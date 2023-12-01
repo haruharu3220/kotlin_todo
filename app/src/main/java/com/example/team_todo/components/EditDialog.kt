21ddd2468f9797d971d80fba20446773c7e09067
Hayato <hayato.314.nico@gmail.com>
2023-12-02 01:47:25 +0900

    AlertDialogの雛形作成

---
+++ app/src/main/java/com/example/team_todo/components/EditDialog.kt
@@ -0,0 +1,14 @@
+package com.example.team_todo.components
+
+import androidx.compose.material.AlertDialog
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EditDialog() {
+    AlertDialog(
+        onDismissRequest = {/*Todo*/},
+        title = {},
+        text = {},
+        buttons = {},
+    )
+}
