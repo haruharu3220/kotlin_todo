df0037679704dda57fcbfff5edb07d64a0701bdf
Hayato <hayato.314.nico@gmail.com>
2023-12-03 01:50:25 +0900

    タスク一覧を表示

--- app/src/main/java/com/example/team_todo/MainActivity.kt
+++ app/src/main/java/com/example/team_todo/MainActivity.kt
@@ -18,9 +18,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.rounded.ShoppingCart
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,6 +30,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.team_todo.components.EditDialog
+import com.example.team_todo.components.TaskList
 import com.example.team_todo.ui.theme.Team_todoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -50,52 +49,52 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
 
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
+//                    Column(
+//                        horizontalAlignment = Alignment.CenterHorizontally,
+//                        verticalArrangement = Arrangement.Center
+//                    ) {
                         MainContent()
-                        Greeting("Android")
-                        Greeting("Test")
-                        MessageCard("メッセージカードです")
-                    }
-                    Row(verticalAlignment = Alignment.Bottom) {
-                        Image(
-                            painter = painterResource(id = R.drawable.neco),
-                            contentDescription = stringResource(id = R.string.neco_content_description),
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                .size(200.dp)
-                                .clip(CircleShape)
-                        )
-                        Column() {
-                            Text("猫です")
-                            Text("かわいいです")
-                        }
-                    }
+//                        Greeting("Android")
+//                        Greeting("Test")
+//                        MessageCard("メッセージカードです")
+//                    }
+//                    Row(verticalAlignment = Alignment.Bottom) {
+//                        Image(
+//                            painter = painterResource(id = R.drawable.neco),
+//                            contentDescription = stringResource(id = R.string.neco_content_description),
+//                            contentScale = ContentScale.Crop,
+//                            modifier = Modifier
+//                                .size(200.dp)
+//                                .clip(CircleShape)
+//                        )
+//                        Column() {
+//                            Text("猫です")
+//                            Text("かわいいです")
+//                        }
+//                    }
 
-                    Box {
-                        Column() {
-                            Image(
-                                painter = painterResource(id = R.drawable.neco),
-                                contentDescription = stringResource(id = R.string.neco_content_description),
-                                contentScale = ContentScale.Crop,
-                                modifier = Modifier
-                                .size(100.dp)
-                            )
-                            Image(
-                                painter = painterResource(id = R.drawable.dog),
-                                contentDescription = stringResource(id = R.string.neco_content_description),
-                                contentScale = ContentScale.Crop,
-                                modifier = Modifier
-                                    .size(100.dp)
-                            )
-                        }
-                        Icon(
-                            Icons.Rounded.ShoppingCart,
-                            contentDescription = stringResource(id = R.string.shopping_cart_content_desc)
-                        )
-                    }
+//                    Box {
+//                        Column() {
+//                            Image(
+//                                painter = painterResource(id = R.drawable.neco),
+//                                contentDescription = stringResource(id = R.string.neco_content_description),
+//                                contentScale = ContentScale.Crop,
+//                                modifier = Modifier
+//                                .size(100.dp)
+//                            )
+//                            Image(
+//                                painter = painterResource(id = R.drawable.dog),
+//                                contentDescription = stringResource(id = R.string.neco_content_description),
+//                                contentScale = ContentScale.Crop,
+//                                modifier = Modifier
+//                                    .size(100.dp)
+//                            )
+//                        }
+//                        Icon(
+//                            Icons.Rounded.ShoppingCart,
+//                            contentDescription = stringResource(id = R.string.shopping_cart_content_desc)
+//                        )
+//                    }
 
 
 
@@ -141,7 +140,14 @@ fun MainContent(viewModel: MainViewModel = hiltViewModel()){
         }
 
     }) {
-        
-    }
+
+//      https://developer.android.com/jetpack/compose/lists?hl=ja
+        val tasks by viewModel.tasks.collectAsState(initial = emptyList())
+            TaskList(
+                tasks = tasks,
+                onClickRow = {},
+                onClickDelete = {},
+            )
+        }
     
 }
